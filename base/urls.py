98db2e983ba2ade12d5727e7de89a409387d65b1
Samuel V <samuelveiga@edu.unifor.br>
2022-10-24 15:58:07 -0300

    submission project

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,17 @@ from . import views
 
 urlpatterns = [
     path("", views.home_page, name="home"),
-    path("event/<int:pk>/", views.event_page, name="event")
+    path("user/<int:pk>/", views.user_page, name="profile"),
+    path("event/<int:pk>/", views.event_page, name="event"),
+    path(
+        "registration-confirmation/<int:pk>/",
+        views.registration_confirmation,
+        name="registration-confirmation",
+    ),
+    path("account/", views.account_page, name="account"),
+    path(
+        "project-submission/<int:pk>/",
+        views.project_submission,
+        name="project-submission",
+    ),
 ]
--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import User, Event, Submission
+from .forms import SubmissionForm
 
 # Create your views here.
 
@@ -10,6 +11,39 @@ def home_page(request):
     context = {"users": users, "events": events}
     return render(request, "home.html", context)
 
+
+def user_page(request, pk):
+    user = User.objects.get(id=pk)
+    return render(request, "profile.html", {"user": user})
+
+
+def account_page(request):
+    return render(request, "account.html", {"user": request.user})
+
+
 def event_page(request, pk):
     event = Event.objects.get(id=pk)
-    return render(request, 'event.html', {'event': event})
+    return render(request, "event.html", {"event": event})
+
+
+def registration_confirmation(request, pk):
+    event = Event.objects.get(id=pk)
+    try:
+        event.participants.add(request.user)
+        return redirect("event", pk=event.id)
+    except:
+        return render(request, "event_confirmation.html", {"event": event})
+
+
+def project_submission(request, pk):
+    event = Event.objects.get(id=pk)
+    form = SubmissionForm()
+    if request.method == "POST":
+        form = SubmissionForm(request.POST)
+        if form.is_valid():
+            submission = form.save(commit=False)
+            submission.participant = request.user
+            submission.event = event
+            submission.save()
+            return redirect("account")
+    return render(request, "submit_form.html", {"event": event, "form": form})
